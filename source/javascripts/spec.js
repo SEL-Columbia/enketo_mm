c94c3dc80452092c99004d8817d667278c18bbd0
Alex Dorey <dorey415@gmail.com>
2012-09-06 21:20:46 -0400

    added some notes to the spec page

--- source/javascripts/spec.js
+++ source/javascripts/spec.js
@@ -2,6 +2,12 @@
 //= require "jasmine-html"
 //= require_tree "./spec"
 
+// Take a look at this page to learn about jasmine testing with coffeescript
+//  - http://coffeescriptcookbook.com/chapters/testing/testing_with_jasmine
+
+// one can modify tests in this file (for example):
+//  - source/javascripts/spec/common.js.coffee
+
 (function() {
   var jasmineEnv = jasmine.getEnv();
   jasmineEnv.updateInterval = 1000;
